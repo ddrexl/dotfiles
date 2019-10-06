2a3e6d66b87ff0f501daac01fca281ec6d720c81
Dominik Drexl <domdrexl@gmail.com>
2019-10-07 00:36:54 +0200

    reworked git hooks

--- git_template/hooks/ctags.sh
+++ git_template/hooks/ctags.sh
@@ -4,5 +4,5 @@ PATH="/usr/local/bin:$PATH"
 dir="`git rev-parse --git-dir`"
 trap 'rm -f "$dir/$$.tags"' EXIT
 git ls-files | \
-  ctags --tag-relative --fields=+l -L - -f"$dir/$$.tags"
+  ctags --tag-relative --fields=+l -L - -f"$dir/$$.tags" --languages=-javascript,sql,tex
 mv "$dir/$$.tags" "$dir/tags"
