f5feeec1c40ae0fda74daac0b9e68f5529580a23
Dominik Drexl <domdrexl@gmail.com>
2025-12-17 15:24:30 +0100

    add configure github cli function

--- install.sh
+++ install.sh
@@ -209,6 +209,16 @@ configure_kubernetes_tools() {
 
 }
 
+configure_github_cli() {
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo apt update
+    sudo apt install gh
+
+    echo choose "GitHub.com", "HTTPS", "Authenticate Git with your GitHub credentials=YES" and "Login with a web browser"
+    gh auth login
+}
+
 help() {
     echo "Install and configure the dotfiles
     -h|--help               show this help
